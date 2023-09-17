86910c8e5209e8d6f956909bcba516d4525ab451
Pham Duc Phuong <phuong.mom33@gmail.com>
2023-09-17 21:02:24 +0700

    create vite

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-comment">
+        Trong file index.css<br></br>
+        ông tìm hiểu về @layer và @apply của tailwind<br></br>
+        để làm gọn cái class của tailwind lại nha<br></br>
+        tui có làm ví dụ className="text-comment" ở trong "app.tsx và index.css"<br></br>
+      </h1>
+    </div>
+  )
+}
+
+export default App
